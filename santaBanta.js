e4a8acb258d8c3db4afcf0a4926f6376c0abb40f
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-12-26 21:42:14 +0530

    setup load

---
+++ santaBanta.js
@@ -0,0 +1,16 @@
+let video;
+let handpose;
+let handPoses;
+let handPose;
+let handConfidence;
+
+let imageArray = [];
+let counter = 3;
+let imageIndex = 0;
+let noImage = true;
+let pastX;
+let pastY;
+
+function preload() {
+    imageArray = [fredrick, molecules, orbital];
+}
