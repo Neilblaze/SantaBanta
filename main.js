4085065c0dbbec53402b96c6bb60d44afd746bcb
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-12-27 15:33:41 +0530

    vars init

---
+++ main.js
@@ -0,0 +1,22 @@
+//Initializing variables
+let video;
+let handpose;
+let handPoses;
+let handPose;
+let handConfidence;
+let lastGesture = "middle";
+let THRESHOLD = 0.80;
+
+//variables declaration for image loading
+let imageArray = [];
+let counter = 3;
+let imageIndex = 0;
+let currentImage;
+// objects prefactor init
+let tree;
+let snowman;
+let snow;
+let holidays;
+let noImage = true;
+let pastX;
+let pastY;
